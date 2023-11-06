fd9da5febcfb52edd2f68051729908492813f224
Benerman <benjaminwlarson@gmail.com>
2023-11-06 10:28:23 -0500

    finishing touches

--- js/index.js
+++ js/index.js
@@ -23,17 +23,29 @@ $("a").on('click', function(event) {
 });
 
 
-// $('#contactSubmitForm').on("submit", function(e) {
-//     e.preventDefault();
-//     var first = document.getElementById('firstName');
-//     var last = document.getElementById('lastName');
-//     var email = document.getElementById('email');
-//     var phone = document.getElementById('phone');
-//     var subject = document.getElementById('subject');
-//     var message = document.getElementById('message');
+$('#contactSubmitForm').on("submit", function(e) {
+    e.preventDefault();
+    var blankFields = false;
+    var first = document.getElementById('firstName');
+    var last = document.getElementById('lastName');
+    var email = document.getElementById('email');
+    var phone = document.getElementById('phone');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('message');
 
-//     if (.value ==) {
-//         ('border-color', 'red');
-//     }
-
-// });
+    var requireds = [first, last, email, message]
+    requireds.forEach(function(reqField) {
+        if (reqField.value == "" ) {
+            reqField.style.borderColor = 'red';
+            blankFields = true;
+        } else {
+            if (reqField.style.borderColor == 'red') {
+                reqField.style.removeProperty("border-color");
+            }
+        }
+    })
+    if (blankFields === true) {
+        alert("Please fill out the required fields.")
+    };
+    // TODO: implement submission process
+});
